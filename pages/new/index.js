09b11232b71a6f2113ca002483a39bb8c95f2688
Abhay Ratti <rattiabhay@gmail.com>
2022-01-15 19:49:32 -0500

    yes

--- pages/new/index.js
+++ pages/new/index.js
@@ -28,17 +28,52 @@ class PadawanForm extends Component {
 
     setTimezone = (e) => this.setState({timezone: e.target.value})
 
+    setOrganization = (e) => this.setState({organization: e.target.value})
+
+    setAbout = (e) => this.setState({about: e.target.value})
+
+    setEvent = (e) => this.setState({event: e.target.value})
+
+    setTwitter = (e) => this.setState({twitter: e.target.value})
+
+    setGithub = (e) => this.setState({github: e.target.value})
+
     render() {
         return (
         <React.Fragment>
             <div className="input-wrapper">
-                <label>name</label>
+                <label>Name</label>
                 <input onChange = {this.setName} placeholder='name' />
             </div>
 
             <div className="input-wrapper">
                 <label>Timezone</label>
                 <input onChange = {this.setTimezone} placeholder='EST' />
+            </div>
+
+            <div className="input-wrapper">
+                <label>Organization</label>
+                <input onChange = {this.setOrganization} placeholder='organization' />
+            </div>
+
+            <div className="input-wrapper">
+                <label>About</label>
+                <input onChange = {this.setAbout} placeholder='tell us about yourself' />
+            </div>
+
+            <div className="input-wrapper">
+                <label>Event</label>
+                <input onChange = {this.setEvent} placeholder='event' />
+            </div>
+
+            <div className="input-wrapper">
+                <label>Twitter</label>
+                <input onChange = {this.setTwitter} placeholder='Twitter @' />
+            </div>
+
+            <div className="input-wrapper">
+                <label>Github</label>
+                <input onChange = {this.setGithub} placeholder='Github' />
             </div>
         </React.Fragment>
         )
