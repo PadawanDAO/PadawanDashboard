3ed407c47187d772c31a8fce7f705b5330235140
Mayank Jain <mayankj2112@gmail.com>
2022-02-01 13:27:54 -0500

    Bunch O UI Updates
    - Profile images no longer stretch
    - consistent card width
    - Dark mode
    - Better Twitter Icon etc.
    
    Notes: Needs better mobile compatability

--- components/atoms/UserCard/UserCard.tsx
+++ components/atoms/UserCard/UserCard.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./UserCard.module.css";
+
+import IconButton from "@mui/material";
+import { Twitter } from "@mui/icons-material";
 
 function CalcAge(birthday: string) {
 	const dob = new Date(birthday);
@@ -51,12 +54,14 @@ function UserCard(props: UserCardProps) {
 	}
 
 	return (
-		<div className={(styles.card, styles.metal)}>
-			<div className="w-fit max-w-md">
+		<div className={styles.card}>
+			<div className="">
 				{/* header and pfp */}
 				<div className="relative pb-16">
 					<div className="">
-						<div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-full rounded-t-xl p-10" />
+						<div
+							className={`${styles.cardHeader} bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-t-xl p-10`}
+						/>
 					</div>
 					<div className="flex justify-center  ">
 						<div className=" md:w-24 absolute top-8 ">
@@ -71,17 +76,25 @@ function UserCard(props: UserCardProps) {
 					</div>
 				</div>
 
-				<div className="text-center font-semibold text-xl pb-2">
-					<h1>
-						{props.name} | {`${CalcAge(props.birthday)}y/o`} |{" "}
-						{props.timezone}
+				<div className="text-center font-semibold text-xl pt-2 pb-2">
+					<h1 style={{ fontFamily: "Poppins", fontSize: "28px" }}>
+						{props.name}
 					</h1>
+					<span>
+						<h1 className="pt-2">
+							<a href={props.twitter}>
+								<Twitter sx={{ fontSize: "30px", color: "#1DA1F2" }} />
+							</a>
+							&nbsp;&nbsp;
+							{`${CalcAge(props.birthday)}y/o`} | {props.timezone}
+						</h1>
+					</span>
 				</div>
 				<div className="pl-8 pr-8">
-					<div className="flex  space-x-2 justify-center pb-4">
+					<div className="flex space-x-2 justify-center pb-4"></div>
+					<div className="flex space-x-2 justify-center pb-4">
 						{props.events && <Tag value={props.events[0]} />}
 						<Tag value={props.organization} />
-						<Social twitter={props.twitter} />
 					</div>
 
 					<div className="pb-4">
@@ -89,14 +102,17 @@ function UserCard(props: UserCardProps) {
 						<p className="h-40"> {props.about} </p>
 					</div>
 
-					<div className="h-20">
+					<div className="pt-5">
 						<h1 className="text-3xl font-semibold">Skills </h1>
 
 						<div className="flex space-x-4 pt-2">
 							{props.skills &&
-								props.skills.map((skill: string, index: number) => (
-									<Tag key={index} value={skill} />
-								))}
+								props.skills.map(
+									(skill: string, index: number) =>
+										skill.replace(" ", "") !== "" && (
+											<Tag key={index} value={skill} />
+										)
+								)}
 						</div>
 					</div>
 				</div>
@@ -107,23 +123,4 @@ function UserCard(props: UserCardProps) {
 	);
 }
 
-function Social(props: { twitter: string }) {
-	return (
-		<div className="hover:cursor-pointer">
-			<Link href={`https://twitter.com/${props.twitter}`} passHref>
-				<a target="_blank">
-					<svg
-						fill="#000000"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 50 50"
-						width="40px"
-						height="40px"
-					>
-						<path d="M50.061,10.438c-1.846,0.818-3.826,1.369-5.908,1.62c2.125-1.273,3.757-3.29,4.523-5.688c-1.986,1.177-4.19,2.033-6.531,2.493c-1.874-2-4.547-3.247-7.504-3.247c-5.68,0-10.284,4.604-10.284,10.282c0,0.805,0.092,1.589,0.269,2.343C16.08,17.812,8.502,13.718,3.429,7.497c-0.885,1.522-1.391,3.289-1.391,5.172c0,3.567,1.812,6.713,4.574,8.56c-1.688-0.054-3.271-0.517-4.657-1.288c0,0.044,0,0.086,0,0.131c0,4.984,3.544,9.134,8.245,10.084c-0.86,0.236-1.769,0.36-2.707,0.36c-0.664,0-1.309-0.064-1.938-0.186c1.313,4.081,5.108,7.06,9.607,7.143c-3.517,2.757-7.951,4.399-12.77,4.399c-0.833,0-1.649-0.048-2.452-0.144c4.548,2.919,9.956,4.619,15.762,4.619c18.915,0,29.26-15.668,29.26-29.252c0-0.448-0.011-0.894-0.03-1.332C46.94,14.313,48.684,12.5,50.061,10.438z" />
-					</svg>
-				</a>
-			</Link>
-		</div>
-	);
-}
 export default UserCard;
